7c0fdd5415c9714072bda8a956c347152bc466c9
pitzzahh <pitzzahh@araopj.tech>
2023-03-22 19:49:11 +0800

    refactor: creating create table query

--- app/src/main/java/tech/araopj/backend/database/DatabaseHelper.java
+++ app/src/main/java/tech/araopj/backend/database/DatabaseHelper.java
@@ -1,41 +1,35 @@
 package tech.araopj.backend.database;
 
+import android.util.Log;
 import android.content.Context;
+import java.lang.reflect.Field;
+import androidx.annotation.Nullable;
+import tech.araopj.backend.model.Todo;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
-
-import androidx.annotation.Nullable;
-
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import tech.araopj.backend.model.Todo;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    private Context context;
+    private final Context context;
     private final String TABLE_NAME = "todo";
-
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, "todo.db", null, 1);
+        this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(String.format("CREATE TABLE %s (", TABLE_NAME));
-        List<String> classFields = getClassFields(Todo.class);
-        for (int i = 0; i < classFields.size(); i++) {
-            String classField = classFields.get(i);
-            if (i == 0) {
-                stringBuilder.append(String.format("%s %s PRIMARY KEY AUTOINCREMENT", classField, "INTEGER"));
+        stringBuilder.append(String.format("CREATE TABLE %s(", TABLE_NAME));
+        Field[] classFields = getClassFields(Todo.class);
+        for (int i = 0; i < classFields.length; i++) {
+            Field classField = classFields[i];
+            if (classField.getName().equals("id")) {
+                stringBuilder.append(String.format("%s %s PRIMARY KEY AUTOINCREMENT", classField.getName(), "INTEGER"), 0, 0);
             }
-            stringBuilder.append(String.format("%s %s", classField, "TEXT"));
-            if (i < classFields.size() - 1) {
+            else stringBuilder.append(String.format("%s %s", classField.getName(), "TEXT"));
+            if (i < classFields.length - 1) {
                 stringBuilder.append(",");
             }
         }
@@ -52,12 +46,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public List<String> getClassFields(Class<?> clazz) {
-        List<String> columnNames = new ArrayList<>();
-        for (Field field : clazz.getFields()) {
-            columnNames.add(field.getName());
-        }
-        return columnNames;
+    public Field[] getClassFields(Class<?> clazz) {
+        return clazz.getDeclaredFields();
     }
 
     public Context getContext() {
