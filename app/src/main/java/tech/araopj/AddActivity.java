126d77c0882488b2b11f0294d193e3b4340985f7
pitzzahh <pitzzahh@araopj.tech>
2023-03-22 19:46:04 +0800

    add: adding todo to db

--- app/src/main/java/tech/araopj/AddActivity.java
+++ app/src/main/java/tech/araopj/AddActivity.java
@@ -1,41 +1,49 @@
 package tech.araopj;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import java.time.LocalDateTime;
-
-import tech.araopj.backend.database.DatabaseHelper;
 import tech.araopj.backend.model.Todo;
+import androidx.appcompat.app.AppCompatActivity;
+import tech.araopj.backend.database.DatabaseHelper;
 import tech.araopj.backend.todo.service.ToDoService;
+import com.google.android.material.snackbar.Snackbar;
 
 public class AddActivity extends AppCompatActivity {
 
     private EditText todo;
     private EditText deadline;
-    private Button addButton;
-
-    private DatabaseHelper databaseHelper;
     private ToDoService toDoService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        databaseHelper = new DatabaseHelper(this);
+        DatabaseHelper databaseHelper = new DatabaseHelper(this);
         toDoService = new ToDoService(databaseHelper);
         setContentView(R.layout.activity_add);
 
         todo = findViewById(R.id.todo);
         deadline = findViewById(R.id.deadline);
-        addButton = findViewById(R.id.addTodo);
+        Button addButton = findViewById(R.id.addTodo);
 
         addButton.setOnClickListener(view -> {
-
+            boolean isValid = checkInputs(view, todo, deadline);
+            if (isValid) toDoService.addToDo(new Todo(todo.getText().toString().trim(), deadline.getText().toString().trim()));
         });
 
+    }
+
+    private boolean checkInputs(View view, EditText todo, EditText deadline) {
+        if(todo.getText().toString().isEmpty()) {
+            Snackbar.make(view, "ToDo cannot be empty", Snackbar.LENGTH_SHORT).show();
+            return false;
+        }
+        if(deadline.getText().toString().isEmpty()) {
+            Snackbar.make(view, "deadline cannot be empty", Snackbar.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 
 
